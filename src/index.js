e6c854168651567a027c0d8c5aa13760b9f724a9
cdonal91 <89680346+cdonal91@users.noreply.github.com>
2021-09-25 19:56:37 +1000

    Fixing up html layout and adding wind, humidity, and weather icon

--- src/index.js
+++ src/index.js
@@ -28,8 +28,19 @@ function displayWeatherCondition(response) {
   document.querySelector("#temperature").innerHTML = Math.round(
     response.data.main.temp
   );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
   document.querySelector("#description").innerHTML =
-    response.data.weather[0].main;
+    response.data.weather[0].description;
+
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
 }
 
 function searchCity(city) {
