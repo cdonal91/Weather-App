5816afbdcb7b233d2e583d2bc5aa58761472f35e
cdonal91 <89680346+cdonal91@users.noreply.github.com>
2021-09-26 20:20:35 +1000

    Updating forecast

--- src/index.js
+++ src/index.js
@@ -23,6 +23,37 @@ function formatDate(date) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  let days = ["Thu", "Fri", "Sat", "Sun"];
+  let forecastHTML = `<div class="row">`;
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `
+    <div class="col-2">
+    <div class="weather-forecast-date">${day}</div>
+    <img
+    src="http://openweathermap.org/img/wn/50d@2x.png"
+    alt=""
+    width="40"/>
+    <div class="weather-forecast-temps">
+      <span class="weather-forecast-temp-max"> 18° </span>
+      <span class="weather-forecast-temp-min"> 12°</span>
+    </div>
+    </div>
+    `;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
 
@@ -42,6 +73,8 @@ function displayWeatherCondition(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+
+  getForecast(response.data.coord);
 }
 
 function searchCity(city) {
